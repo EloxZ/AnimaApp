42cd8c9aecbff58664a0719de81c41c47fde58f8
EloxZ <45355879+EloxZ@users.noreply.github.com>
2022-01-10 17:28:07 +0100

    Limpieza

--- app/src/main/java/com/example/characterapp/clases/Hechicero.java
+++
@@ -1,9 +0,0 @@
-package com.example.characterapp.clases;
-
-import com.example.characterapp.Clase;
-
-public class Hechicero extends Clase {
-    public Hechicero(){
-        super("Hechicero", 20,3,2,3,1, 50,2,5,20,3,2,2,2,2,2,1,5,0,0,0,100,3,0,0,0,0,0,10);
-    }
-}
