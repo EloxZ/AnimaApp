50524623873fb03de1725eb5403f822a1e3a0315
AaronHDN <62114009+AaronHDN@users.noreply.github.com>
2022-01-10 05:24:54 +0100

    Pantalla de PDs

---
+++ app/src/main/java/com/example/characterapp/PDsActivity.java
@@ -0,0 +1,14 @@
+package com.example.characterapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PDsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pds2);
+        setTitle("owo");
+    }
+}
