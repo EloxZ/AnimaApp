0293ec0c4e442d92a1d61b53757bafd88a51c94f
EloxZ <45355879+EloxZ@users.noreply.github.com>
2022-01-10 11:06:53 +0100

    Base de datos

---
+++ app/src/main/java/com/example/characterapp/DatabaseHelper.java
@@ -0,0 +1,75 @@
+package com.example.characterapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "anima.db";
+    private Context context;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        InputStream inputStream = context.getResources().openRawResource(R.raw.script);
+        try {
+            String queries = CharStreams.toString(new InputStreamReader(
+                    inputStream, Charsets.UTF_8));
+
+            for (String query : queries.split(";")) {
+                Log.d("DB", query);
+                db.execSQL(query);
+            }
+        } catch (IOException e) {
+            Log.e("Error", e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS Armaduras");
+        db.execSQL("DROP TABLE IF EXISTS Armas");
+        db.execSQL("DROP TABLE IF EXISTS Caracteristica");
+        db.execSQL("DROP TABLE IF EXISTS Clases");
+        db.execSQL("DROP TABLE IF EXISTS Personajes");
+        onCreate(db);
+    }
+
+    public boolean addCharacter(String n, String r, int lvl) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Nombre", n);
+        contentValues.put("Raza", r);
+
+        long result = db.insert("Personajes", null, contentValues);
+
+        //if date as inserted incorrectly it will return -1
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public Cursor getListContents(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + "Personajes", null);
+        return data;
+    }
+}
--- app/src/main/java/com/example/characterapp/MainActivity.java
+++ app/src/main/java/com/example/characterapp/MainActivity.java
@@ -2,21 +2,26 @@ package com.example.characterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton botonAddChar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("AnimaApp");
         setContentView(R.layout.activity_main);
+        DatabaseHelper db = new DatabaseHelper(this);
         botonAddChar = (FloatingActionButton) findViewById(R.id.botonAddChar);
         botonAddChar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +31,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(addCharIntent);
             }
         });
+
+        //Esto lo puse para probar que funcionaba gucci
+        //Cursor c = db.getListContents();
+        //c.moveToFirst();
+        //int i = c.getColumnIndex("Nombre");
+
+        //Toast.makeText(this, c.getString(i), Toast.LENGTH_LONG).show();
     }
 }
